27e321536d9de325ab8cd41c0fbcafaa9417099f
Pedro Ladeira <pedr.ladeira@gmail.com>
2016-08-19 21:31:57 +0100

    htaccess

--- app/languages/pt.php
+++ app/languages/pt.php
@@ -1,3 +1,6 @@
 <?php
 
-define('header_navigation_started', 'Get Started<');
+define('header_navigation_started', 'Get Started');
+define('header_navigation_helpers', 'CSS Ajudas');
+define('header_navigation_components', 'CSS Components');
+define('header_navigation_javascript', 'JavaScript');
