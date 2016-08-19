b41a3cb0ab59a238f38bcf0a36f093452dbf7229
Pedro Ladeira <pedr.ladeira@gmail.com>
2016-08-19 23:16:38 +0100

    modal functionality

--- app/views/javascript/modal.php
+++ app/views/javascript/modal.php
@@ -19,8 +19,8 @@
                             &lt;!-- Use a hyperlink 'href=""' -->
                             &lt;a class="button button-white button-large openModal" href="#modal">Show Modal&lt;/a>
 
-                            &lt;!-- Use a button 'data-target=""' -->
-                            &lt;button class="button button-white button-large openModal" data-target="#modal">Show Modal&lt;/button>
+                            &lt;!-- Use a button 'stewed-modal=""' -->
+                            &lt;button class="button button-white button-large openModal" stewed-modal="modal">Show Modal&lt;/button>
 
                             &lt;!-- Structure -->
                             &lt;div id="modal" class="container modal">
@@ -70,7 +70,7 @@
     </div>
     <div id="openModal" class="container modal">
         <div class="modal-head">
-            <h2>Modal Header</h2>
+            <h2>Modal Header (with effect)</h2>
             <p>Press key <strong>ESC</strong></p>
         </div>
         <div class="modal-body">
@@ -84,7 +84,7 @@
 
     <div id="openModalNo" class="container modal">
         <div class="modal-head">
-            <h2>Modal Header</h2>
+            <h2>Modal Header  (without effect)</h2>
         </div>
         <div class="modal-body">
             <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
--- build/components/modal/modal.js
+++ build/components/modal/modal.js
@@ -1,6 +1,11 @@
+//DEBUG
+console.log('Modal (v0.1.1)');
+
 //Modal
 function Modal(modelID) {
+    var _self = this;
     this.modal = document.getElementById(modelID);
+    this.isOpen = false;
 
     this.backgroundColor = '#000';
     this.opacityOverlay  = 0.6; 
@@ -9,26 +14,56 @@ function Modal(modelID) {
     this.durationIn      = 0.477;
     this.durationOut     = 0.177;
 
+    //construct Object
+    var buttons = this.modal.getElementsByClassName('modal-close');
+    for (var i = 0; i < buttons.length; i++) {
+        var btn_close = buttons[i];
+        btn_close.addEventListener('click', function(){
+            _self.hide();
+        });
+    };
+
+
     this.show = function () {
         //set background color
         this.modal.style.visibility = 'visible';
+        this.isOpen = true;
     };
     this.hide = function(){
         this.modal.style.visibility = 'hidden';
+        this.isOpen = false;
     };
+
+
+
 };
 
 //SHOW MODAL
 ! function(){
+
+    var memory_modals = [];
     
     var elems = document.querySelectorAll('[stewed-modal]');
     for (var i = 0; i < elems.length; i++) {
         var elem = elems[i];
-        var modelID = elem.getAttribute('stewed-modal');
+        var modalID = elem.getAttribute('stewed-modal');
+        memory_modals[modalID] = new Modal(modalID);
+        
+        //element click
         elem.addEventListener('click', function(){
-            var modal = new Modal(modelID);
-            modal.backgroundColor = '#FFF';
-            modal.show();
+            var current_modal = this.getAttribute('stewed-modal');
+            
+            var modal = memory_modals[current_modal];
+            if(modal.isOpen){
+                modal.hide();
+            }else{
+                modal.show();
+            }
+            //new Modal().show(); 
         });
+        //element find class modal-close
+        var closeBtn = elem.getElementsByClassName(elem.getAttribute('stewed-modal'));
+
+
     }
 }();
