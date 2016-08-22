82d2bf612981140ace9dff4ca3cb0dd81833f8af
Mauro Reis Vieira <mv010690@gmail.com>
2016-08-22 23:22:02 +0100

    Change view CSS Components

--- app/views/partials/menu_components.php
+++ app/views/partials/menu_components.php
@@ -1,8 +1,8 @@
-<div class="">
+<div class="css-components">
 	<div class="container">
 		<div class="row-space">
 			<div class="large-12 medium-12 small-12">
-				<nav class="navigation css-components">
+				<nav class="navigation">
 					<ul class="navigation__horizontal">
 						<li><a href="" class="color-pink"><?= partial_menu_components_badges; ?></a></li>
 						<li><a href=""><?= partial_menu_components_buttons; ?></a></li>
