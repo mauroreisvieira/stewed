12d54ac9d2fd9a89249f6e11415138eb40e47b90
Mauro Reis Vieira <mv010690@gmail.com>
2016-09-16 11:54:33 +0100

    remove folder app

--- app/languages/pt.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('header_navigation_started', 'Get Started');
-define('header_navigation_helpers', 'CSS Ajudas');
-define('header_navigation_components', 'CSS Components');
-define('header_navigation_javascript', 'JavaScript');
