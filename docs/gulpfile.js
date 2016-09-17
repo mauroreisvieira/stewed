3fb7a18077538cfa9a97d2e66464f4cfae84c567
Mauro Reis Vieira <mv010690@gmail.com>
2016-09-17 18:55:35 +0100

    more docs
    :P

--- docs/gulpfile.js
+++ docs/gulpfile.js
@@ -10,33 +10,96 @@ COMPACT = 'compact',
 COMPRESSED = 'compressed';
 
 gulp.task('default', function () {
-    return gulp.src('assets/css/sass/*.scss')
-    .pipe(sass({outputStyle: EXPANDED}).on('error', sass.logError))
-    .pipe(gulp.dest('assets/css'));
+	return gulp.src('assets/css/sass/*.scss')
+	.pipe(sass({outputStyle: EXPANDED}).on('error', sass.logError))
+	.pipe(gulp.dest('assets/css'));
 });
 
 gulp.task('watch', function () {
-    gulp.watch('assets/css/sass/*.scss', ['default']);
+	gulp.watch('assets/css/sass/*.scss', ['default']);
 });
 
 
 /**
  * HTML FILES
  */
-gulp.task('html', function() {
-	gulp.src([
-	    'views/layout/header.html',
-	    'views/home.html', 
-	    'views/layout/footer.html'
-	])
-	.pipe(concat('index.html'))
-	.pipe(gulp.dest('.'));
+ gulp.task('html', function() {
+ 	gulp.src([
+ 		'views/layout/header.html',
+ 		'views/home.html', 
+ 		'views/layout/footer.html'
+ 		])
+ 	.pipe(concat('index.html'))
+ 	.pipe(gulp.dest('.'));
 
-	gulp.src([
-	    'views/layout/header.html',
-	    'views/helpers/utilities.html', 
-	    'views/layout/footer.php'
-	])
-	.pipe(concat('helpers.html'))
-	.pipe(gulp.dest('.'));
-});
+ 	gulp.src([
+ 		'views/layout/header.html',
+ 		'views/helpers/utilities.html', 
+ 		'views/layout/footer.html'
+ 		])
+ 	.pipe(concat('helpers.html'))
+ 	.pipe(gulp.dest('.'));
+
+ 	gulp.src([
+ 		'views/layout/header.html',
+ 		'views/components.html', 
+ 		'views/components/badges.html', 
+ 		'views/layout/footer.html'
+ 		])
+ 	.pipe(concat('badges.html'))
+ 	.pipe(gulp.dest('.'));
+
+ 	gulp.src([
+ 		'views/layout/header.html',
+ 		'views/components.html', 
+ 		'views/components/breadcrumbs.html', 
+ 		'views/layout/footer.html'
+ 		])
+ 	.pipe(concat('breadcrumbs.html'))
+ 	.pipe(gulp.dest('.'));
+
+ 	gulp.src([
+ 		'views/layout/header.html',
+ 		'views/components.html', 
+ 		'views/components/buttons.html', 
+ 		'views/layout/footer.html'
+ 		])
+ 	.pipe(concat('buttons.html'))
+ 	.pipe(gulp.dest('.'));
+
+ 	gulp.src([
+ 		'views/layout/header.html',
+ 		'views/components.html', 
+ 		'views/components/cards.html', 
+ 		'views/layout/footer.html'
+ 		])
+ 	.pipe(concat('cards.html'))
+ 	.pipe(gulp.dest('.'));
+
+ 	gulp.src([
+ 		'views/layout/header.html',
+ 		'views/components.html', 
+ 		'views/components/forms.html', 
+ 		'views/layout/footer.html'
+ 		])
+ 	.pipe(concat('forms.html'))
+ 	.pipe(gulp.dest('.'));
+
+ 	gulp.src([
+ 		'views/layout/header.html',
+ 		'views/components.html', 
+ 		'views/components/grid.html', 
+ 		'views/layout/footer.html'
+ 		])
+ 	.pipe(concat('grid.html'))
+ 	.pipe(gulp.dest('.'));
+
+ 	gulp.src([
+ 		'views/layout/header.html',
+ 		'views/components.html', 
+ 		'views/components/tables.html', 
+ 		'views/layout/footer.html'
+ 		])
+ 	.pipe(concat('tables.html'))
+ 	.pipe(gulp.dest('.'));
+ });
