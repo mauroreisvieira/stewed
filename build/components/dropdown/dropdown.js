0644725ff1a14c74988fb3cd6bf10563f324ed54
Mauro Reis Vieira <mv010690@gmail.com>
2016-10-14 16:44:03 +0100

    Adding Dropdown
    Adding file utilities

--- build/components/dropdown/dropdown.js
+++ build/components/dropdown/dropdown.js
@@ -1,2 +1,60 @@
-"use strict"
+    var Dropdown = function(selected, id) {
+        'use strict';
+
+        this.dropdown = selected;
+        this.id = document.getElementById(id);
+
+
+        this.show = function(){
+
+            if (!this.id.classList.contains('dropdown--visible')) {
+                var elements = document.getElementsByClassName('dropdown--visible');
+
+                if (elements.length > 0) {
+                    elements[0].classList.remove("dropdown--visible");
+                }
+                this.id.className += " dropdown--visible";
+
+            } else {
+                this.id.className += " dropdown--animatable";
+                this.id.classList.remove("dropdown--visible");
+            }
+        }
+
+        this.id.addEventListener("transitionend", function(){
+            if (this.classList.contains('dropdown--animatable')) {
+                this.classList.remove('dropdown--animatable');
+            }
+        });
+
+    };
+
+
+    ! function(){
+        'use strict';
+
+        /* get dropdown item */
+        var elems = document.querySelectorAll('[stewed-dropdown]');
+
+        for (var i = 0; i < elems.length; i++) {
+
+            var myDropdown = elems[i];
+
+            /* on click in tab */
+            myDropdown.addEventListener('click', function() {
+                'use strict';
+
+                var id = this.getAttribute('stewed-dropdown')
+                var dropdown = new Dropdown(this, id);
+
+                if ( !this.classList.contains('disabled') ) {
+                    dropdown.show();
+                }
+
+            });        
+
+        }
+
+    }();
+
 
