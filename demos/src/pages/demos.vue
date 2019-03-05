999972ed4122572310103a47dba7e49fc0dccce5
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2019-03-05 16:52:23 +0000

    feat: prepare new version.

--- demos/src/pages/demos.vue
+++ demos/src/pages/demos.vue
@@ -35,9 +35,7 @@
                 const hedingDemo = this.$el.querySelector('h3');
                 const parentDiv = hedingDemo.parentNode;
                 const htmlDemo = this.$el.querySelector('.language-html').textContent;
-                // console.log(this.$el.querySelectorAll('pre.language-html'));
                 [...this.$el.querySelectorAll('pre.language-html')].map(e => {
-                    console.log(e);
                     const demo = document.createElement('div');
                     demo.innerHTML = e.textContent;
                     demo.classList.add('demo');
