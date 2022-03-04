60d03cc959ed93fa639179c8959ba297381847bd
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2022-03-04 16:47:28 +0000

    chore: add demo for Menu

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -2,42 +2,48 @@ import * as React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 
+import { Menu } from '@stewed/menu';
+
 const Home: React.FC = (): React.ReactElement => (
     <>
         <Head>
             <title>Stewed</title>
         </Head>
-        <h2>Feedback</h2>
-        <ul>
-            <li>
+        <Menu>
+            <Menu.Separator>Feedback</Menu.Separator>
+            <Menu.Item>
                 <Link href="/components/alert" passHref>
                     Alert
                 </Link>
-            </li>
-            <li>
+            </Menu.Item>
+            <Menu.Item>
                 <Link href="/components/badge" passHref>
                     Badge
                 </Link>
-            </li>
-        </ul>
-        <h2>Forms</h2>
-        <ul>
-            <li>
+            </Menu.Item>
+            <Menu.Separator>Navigation</Menu.Separator>
+            <Menu.Item>
+                <Link href="/components/menu" passHref>
+                    Menu
+                </Link>
+            </Menu.Item>
+            <Menu.Separator>Forms</Menu.Separator>
+            <Menu.Item>
                 <Link href="/components/button" passHref>
                     Button
                 </Link>
-            </li>
-            <li>
+            </Menu.Item>
+            <Menu.Item>
                 <Link href="/components/textfield" passHref>
                     Textfield
                 </Link>
-            </li>
-            <li>
+            </Menu.Item>
+            <Menu.Item>
                 <Link href="/components/radio" passHref>
                     Radio
                 </Link>
-            </li>
-        </ul>
+            </Menu.Item>
+        </Menu>
     </>
 );
 
