960dc799ad512aa674200f744409bbbe3b6743e1
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2023-01-15 02:00:19 +0000

    feat(radio): add new component

---
+++ packages/components/src/radio/Radio.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import { classNames } from '@stewed/utils';
+
+import styles from './Base.module.scss';
+
+export interface RadioProps
+    extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'size'> {
+    skin?: 'default' | 'danger';
+    size?: 'sm' | 'md' | 'lg';
+}
+
+export const Radio = React.forwardRef(
+    (
+        {
+            skin = 'default',
+            size = 'md',
+            className,
+            disabled,
+            ...otherProps
+        }: RadioProps,
+        ref: React.Ref<HTMLInputElement>
+    ): React.ReactElement => {
+        const rootClassName = 'radio';
+        const cssClasses = {
+            root: classNames(
+                styles[rootClassName],
+                disabled && styles[`${rootClassName}--disabled`],
+                size !== 'md' && styles[`${rootClassName}--${size}`],
+                skin !== 'default' && styles[`${rootClassName}--${skin}`],
+                className
+            ),
+            control: styles[`${rootClassName}__control`],
+            background: styles[`${rootClassName}__background`],
+        };
+
+        return (
+            <div className={cssClasses.root}>
+                <input
+                    ref={ref}
+                    type="radio"
+                    disabled={disabled}
+                    className={cssClasses.control}
+                    {...otherProps}
+                />
+                <span className={cssClasses.background} />
+            </div>
+        );
+    }
+);
+
+Radio.displayName = 'Radio';
