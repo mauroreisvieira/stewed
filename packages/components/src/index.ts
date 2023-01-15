565a8fbd8181add6cba8a9ee6aca1e4ae7f00e84
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2023-01-15 02:00:27 +0000

    docs: update storybook

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -2,4 +2,5 @@ export { Alert, type AlertProps } from "./alert";
 export { Badge, type BadgeProps } from "./badge";
 export { Button, type ButtonProps } from "./button";
 export { Checkbox, type CheckboxProps } from "./checkbox";
+export { Radio, type RadioProps } from "./radio";
 export { Textfield, type TextfieldProps } from "./textfield";
