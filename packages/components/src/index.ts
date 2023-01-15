c9fdf331be59c3451a28ef1ccd9eb34f9e9f0f5e
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2023-01-15 02:15:48 +0000

    feat(tag): add new component

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -3,4 +3,5 @@ export { Badge, type BadgeProps } from "./badge";
 export { Button, type ButtonProps } from "./button";
 export { Checkbox, type CheckboxProps } from "./checkbox";
 export { Radio, type RadioProps } from "./radio";
+export { Tag, type TagProps } from "./tag";
 export { Textfield, type TextfieldProps } from "./textfield";
---
+++ packages/components/src/tag/Tag.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { classNames } from '@stewed/utils';
+
+import styles from './Base.module.scss';
+
+type TagType = React.HTMLAttributes<HTMLSpanElement> &
+    React.AnchorHTMLAttributes<HTMLAnchorElement>;
+export interface TagProps extends TagType {
+    skin?: 'primary' | 'secondary' | 'info' | 'success' | 'warning' | 'danger';
+    size?: 'sm' | 'md' | 'lg';
+    className?: string;
+    children: React.ReactNode;
+}
+
+export const Tag = ({
+    skin = 'primary',
+    size = 'md',
+    className,
+    children,
+    ...otherProps
+}: TagProps): React.ReactElement => {
+    const { href } = otherProps;
+    const Tag = href ? 'a' : 'span';
+    const rootClassName = 'tag';
+    const cssClasses = {
+        root: classNames(
+            styles[rootClassName],
+            styles[`${rootClassName}--${skin}`],
+            styles[`${rootClassName}--${size}`],
+            className
+        ),
+    };
+
+    return (
+        <Tag className={cssClasses.root} href={href} {...otherProps}>
+            {children}
+        </Tag>
+    );
+};
