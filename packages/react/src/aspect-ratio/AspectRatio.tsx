2f274632cc6685bfbe5e3687b8e58889bf3a0e06
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2023-01-25 20:45:20 +0000

    feat(aspect-ratio): add new component

---
+++ packages/react/src/aspect-ratio/AspectRatio.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { classNames } from '@stewed/utils';
+
+import styles from './Base.module.scss';
+
+export interface AspectRatioProps extends React.HTMLAttributes<HTMLDivElement> {
+    ratio?: '1:1' | '4:3' | '16:9';
+}
+
+export const AspectRatio = ({
+    ratio = '1:1',
+    className,
+    children,
+    ...otherProps
+}: AspectRatioProps): React.ReactElement => {
+    const rootClassName = 'aspect-ratio';
+    const cssClasses = {
+        root: classNames(
+            styles[rootClassName],
+            styles[`${rootClassName}--${ratio.replace(":", "-")}`],
+            className
+        ),
+    };
+
+    return <div className={cssClasses.root} {...otherProps}>{children}</div>;
+};
