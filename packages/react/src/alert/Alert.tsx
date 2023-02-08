267d7e146880b01b27f749d4b36b035352c4fb67
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2023-02-08 17:38:42 +0000

    feat(alert): extend API from native attributes

--- packages/react/src/alert/Alert.tsx
+++ packages/react/src/alert/Alert.tsx
@@ -3,18 +3,34 @@ import { classNames } from '@stewed/utils';
 
 import styles from './Base.module.scss';
 
-export interface AlertProps {
+export interface AlertProps extends React.HTMLAttributes<HTMLDivElement> {
+    /** Will render the bold text shown at the top of the alert. */
     title?: string;
+    /** Change the visual style of the alert. */
     skin?: 'info' | 'success' | 'warning' | 'danger';
-    className?: string;
-    children?: React.ReactNode;
 }
 
+/**
+ * This component displays an alert component.
+ * Alerts component are used to communicate a state that affects a system, feature or page.
+ *
+ * @example
+ * ```tsx
+ * <Alert
+ *   skin="info"
+ *   title="Are you absolutely sure?">
+ *     This action cannot be undone...
+ * </Alert>
+ * ```
+ *
+ * @param props - AlertProps
+ */
 export const Alert = ({
     title,
     skin = 'info',
     className,
     children,
+    ...restProps
 }: AlertProps): React.ReactElement => {
     const rootClassName = 'alert';
 
@@ -29,7 +45,7 @@ export const Alert = ({
     };
 
     return (
-        <div className={cssClasses.root} role="alert">
+        <div {...restProps} className={cssClasses.root} role="alert">
             {title && <div className={cssClasses.title}>{title}</div>}
             <div className={cssClasses.body}>{children}</div>
         </div>
