20179451f5c1527c39fc96f982bb691808f9b53b
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2023-11-17 16:11:50 +0000

    chore: customize the storybook

---
+++ .storybook/manager.ts
@@ -0,0 +1,18 @@
+import { addons } from "@storybook/manager-api";
+import { create } from "@storybook/theming";
+
+const theme = create({
+  base: "light",
+  brandTitle: "Stewed",
+  brandUrl: "/",
+  brandImage: "../assets/images/logo/stewed.svg",
+  colorPrimary: "#6366f1",
+  colorSecondary: "#64748b",
+  textColor: "#1e293b",
+  appBg: "#fff"
+});
+
+addons.setConfig({
+  theme,
+  showToolbar: true,
+});
