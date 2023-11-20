e8fc7f0eabcf68fafa0420a2455d6e4937bc176e
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2023-11-20 14:20:44 +0000

    refactor: improve components to apply classes in children elements

--- packages/react/src/avatar/src/Avatar.tsx
+++ packages/react/src/avatar/src/Avatar.tsx
@@ -1,55 +1,47 @@
-import React from 'react';
+import React from "react";
 // Compound Component
-import { AvatarGroup } from './AvatarGroup';
+import { AvatarGroup } from "./AvatarGroup";
 // Utilities
-import { classNames } from '@stewed/utilities';
+import { classNames } from "@stewed/utilities";
 // Styles
-import styles from './styles/index.module.scss';
+import styles from "./styles/index.module.scss";
 
-export interface AvatarProps extends React.ImgHTMLAttributes<HTMLImageElement> {
-    /** Change the visual style of the avatar. */
-    skin?: 'primary' | 'secondary';
-    /** Changes the size of the avatar. */
-    size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl';
-    /** If true, the avatar will be square instead of circular. */
-    square?: boolean;
+export interface AvatarProps {
+  skin?: "primary" | "secondary";
+  size?: "xs" | "sm" | "md" | "lg" | "xl" | "2xl";
+  appearance?: "circle" | "square";
+  name?: string;
+  className?: string;
+  src?: string;
 }
 
 export function Avatar({
-    size = 'md',
-    skin = 'primary',
-    square,
-    className,
-    src,
-    children,
-    ...otherProps
+  size = "md",
+  skin = "primary",
+  appearance = "circle",
+  className,
+  src,
+  name,
 }: AvatarProps): React.ReactElement {
-    const rootClassName = 'avatar';
-    const cssClasses = {
-        root: classNames(
-            styles[rootClassName],
-            square && styles[`${rootClassName}--square`],
-            styles[`${rootClassName}--${size}`],
-            styles[`${rootClassName}--${skin}`],
-            className
-        ),
-    };
+  const rootClassName = "avatar";
+  const cssClasses = {
+    root: classNames(
+      styles[rootClassName],
+      appearance && styles[`${rootClassName}--${appearance}`],
+      styles[`${rootClassName}--${size}`],
+      styles[`${rootClassName}--${skin}`],
+      className,
+    ),
+    img: styles[`${rootClassName}__img`],
+  };
 
-    return (
-        <div className={cssClasses.root}>
-            {src ? (
-                <img
-                    src={src}
-                    alt={children?.toString() || otherProps.alt}
-                    {...otherProps}
-                />
-            ) : children && typeof children === 'string' ? (
-                children.match(/[A-Z]/g)?.join('').slice(0, 2).toUpperCase()
-            ) : (
-                children
-            )}
-        </div>
-    );
+  const initials = name?.match(/[A-Z]/g)?.join("").slice(0, 2).toUpperCase();
+
+  return (
+    <div className={cssClasses.root}>
+      {src ? <img className={cssClasses.img} src={src} alt={name} /> : initials}
+    </div>
+  );
 }
 
 // Compound component composition
