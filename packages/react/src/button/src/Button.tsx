cf0d94868f93d82d99734e20f59e6827b8acd7ba
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2023-12-12 17:13:50 +0000

    chore: update tag to be polymorphic

--- packages/react/src/button/src/Button.tsx
+++ packages/react/src/button/src/Button.tsx
@@ -28,7 +28,7 @@ export interface ButtonProps<T> extends React.ComponentProps<typeof defaultEleme
    * Changes the size of the button, giving it more or less padding.
    * @default md
    */
-  size?: "sm" | "md" | "lg" | "xl";
+  size?: "xs" | "sm" | "md" | "lg" | "xl";
   /** Slot for icon to display before the button text. */
   leftSlot?: React.ReactNode;
   /** Slot for icon to display after the button text. */
