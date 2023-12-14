c8b349eee28a50269488adb861d26bb106cf7773
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2023-12-14 20:57:44 +0000

    refactor: update radio button

--- packages/react/src/radio/src/Radio.tsx
+++ packages/react/src/radio/src/Radio.tsx
@@ -1,52 +1,64 @@
-import React, { forwardRef } from 'react';
+import React, { forwardRef } from "react";
 // Utilities
-import { classNames } from '@stewed/utilities';
+import { classNames } from "@stewed/utilities";
 // Styles
-import styles from './styles/index.module.scss';
+import styles from "./styles/index.module.scss";
 
-export interface RadioProps
-    extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'size'> {
-    skin?: 'primary' | 'error';
-    size?: 'sm' | 'md' | 'lg';
+export interface RadioProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "size"> {
+  /**
+   * Specifies the visual style of the radio.
+   * @default primary
+   */
+  skin?: "primary" | "error";
+  /**
+   * Specifies the size of the radio.
+   * @default md
+   */
+  size?: "sm" | "md" | "lg";
+  /** Content to be rendered within the radio, usually used for labels. */
+  children?: React.ReactNode;
 }
 
+/**
+ * Radio is a form element used for selecting one option from a list.
+ *
+ * @example
+ * ```tsx
+ * <Radio skin="secondary">Label</Radio>
+ * ```
+ *
+ * @remarks This component props extended from React.InputHTMLAttributes<HTMLInputElement>.
+ *
+ * @param {RadioProps} props - The props for the Radio component.
+ * @returns {React.ReactElement} - The rendered Radio component.
+ */
 export const Radio = forwardRef(
-    (
-        {
-            skin = 'primary',
-            size = 'md',
-            className,
-            disabled,
-            ...otherProps
-        }: RadioProps,
-        ref: React.Ref<HTMLInputElement>
-    ): React.ReactElement => {
-        const rootClassName = 'radio';
-        const cssClasses = {
-            root: classNames(
-                styles[rootClassName],
-                disabled && styles[`${rootClassName}--disabled`],
-                styles[`${rootClassName}--${size}`],
-                styles[`${rootClassName}--${skin}`],
-                className
-            ),
-            control: styles[`${rootClassName}__control`],
-            background: styles[`${rootClassName}__background`],
-        };
+  (
+    { skin = "error", size = "md", className, disabled, children, ...props }: RadioProps,
+    ref: React.Ref<HTMLInputElement>,
+  ): React.ReactElement => {
+    const rootName = "radio";
+    const cssClasses = {
+      root: classNames(
+        styles[rootName],
+        disabled && styles[`${rootName}--disabled`],
+        styles[`${rootName}--${size}`],
+        styles[`${rootName}--${skin}`],
+        className,
+      ),
+      input: styles[`${rootName}__input`],
+      control: styles[`${rootName}__control`],
+      text: styles[`${rootName}__text`],
+    };
 
-        return (
-            <div className={cssClasses.root}>
-                <input
-                  ref={ref}
-                  type="radio"
-                  disabled={disabled}
-                  className={cssClasses.control}
-                  {...otherProps}
-                />
-                <span className={cssClasses.background} />
-            </div>
-        );
-    }
+    return (
+      <label className={cssClasses.root}>
+        <input ref={ref} type="radio" disabled={disabled} className={cssClasses.input} {...props} />
+        <span className={cssClasses.control} />
+        {children && <span className={cssClasses.text}>{children}</span>}
+      </label>
+    );
+  },
 );
 
-Radio.displayName = 'Radio';
+Radio.displayName = "Radio";
