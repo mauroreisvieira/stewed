bf132a4c0c86727d684189f2fcb7de4fe9fff2e5
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2024-02-09 17:22:16 +0000

    fix: line height in thead

--- src/stories/dashboard/src/RecentFeedback.tsx
+++ src/stories/dashboard/src/RecentFeedback.tsx
@@ -158,11 +158,11 @@ export function RecentFeedback(): React.ReactElement {
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
-                        stroke-width="2"
+                        strokeWidth="2"
                         stroke="currentColor">
                         <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
                           d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z"
                         />
                       </svg>
