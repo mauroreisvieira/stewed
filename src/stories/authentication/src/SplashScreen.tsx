5c4ed02c5cd3e401718f0121f9bfbccbcad83fb0
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2024-02-23 16:53:36 +0000

    docs: update splash screen example

--- src/stories/authentication/src/SplashScreen.tsx
+++ src/stories/authentication/src/SplashScreen.tsx
@@ -15,7 +15,15 @@ export function SplashScreen(): React.ReactElement {
     <div style={{ maxWidth: 420 }}>
       <Theme>
         <Card padding={{ inline: "3xl", block: "3xl" }}>
-          <Carousel loop>
+          <Carousel
+            loop={false}
+            showNavigation={false}
+            responsive={{
+              sm: {
+                showNavigation: true,
+                loop: true,
+              },
+            }}>
             {Array.from({ length: 2 }).map((_, key) => (
               <Box key={key} direction="column">
                 <AspectRatio ratio="1:1">
@@ -25,10 +33,28 @@ export function SplashScreen(): React.ReactElement {
                   direction="column"
                   gap="xl"
                   items="center"
-                  padding={{ block: "2xl", inline: "2xl" }}>
-                  <Text as="h3">Purchase Quickly</Text>
-                  <Text size="lg" skin="neutral" alignment="center">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, asperiores?
+                  responsive={{
+                    sm: {
+                      padding: {
+                        block: "3xl",
+                        inline: "2xl",
+                      },
+                    },
+                  }}
+                  padding={{ block: "2xl", inline: "md" }}>
+                  <Text
+                    as="h3"
+                    size="2xl"
+                    alignment="center"
+                    responsive={{
+                      sm: {
+                        size: "4xl",
+                      },
+                    }}>
+                    Purchase Quickly
+                  </Text>
+                  <Text size="lg" skin="neutral" alignment="center" variation="italic">
+                    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, asperiores?"
                   </Text>
                 </Box>
               </Box>
@@ -37,12 +63,10 @@ export function SplashScreen(): React.ReactElement {
 
           <Card.Body>
             <Box gap="lg">
-              <Button size="lg" appearance="outline" fullWidth>
+              <Button appearance="outline" fullWidth>
                 Log in
               </Button>
-              <Button size="lg" fullWidth>
-                Join now
-              </Button>
+              <Button fullWidth>Join now</Button>
             </Box>
           </Card.Body>
         </Card>
