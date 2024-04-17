11fc7ecd5c66bfa975e5558424d6a5122854ecb4
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2024-04-17 11:02:20 +0100

    chore: update lock file

--- .releaserc.js
+++
@@ -1,9 +0,0 @@
-// https://www.npmjs.com/package/semantic-release-config-conventional
-process.env.SR_CONFIG_NPM_PUBLISH = "false"; // prevent the main package to the registry
-
-/** @type import("semantic-release").GlobalConfig */
-const config = {
-  extends: "semantic-release-config-conventional",
-};
-
-module.exports = config;
