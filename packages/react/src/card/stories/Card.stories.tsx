14b0a148352f26c864514a4e4741b1aa27ec996d
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2024-04-24 12:18:45 +0100

    revert: card storybook example

--- packages/react/src/card/stories/Card.stories.tsx
+++ packages/react/src/card/stories/Card.stories.tsx
@@ -2,7 +2,7 @@ import React from "react";
 // Storybook
 import type { Meta, StoryObj } from "@storybook/react";
 // Packages
-import { Theme, Card, Text, Button, useTheme } from "../../index";
+import { Theme, Card, Text } from "../../index";
 
 type Story = StoryObj<typeof Card>;
 
@@ -11,103 +11,14 @@ const meta: Meta<typeof Card> = {
   component: Card,
   decorators: [
     (Story) => (
+      <Theme>
         <Story />
+      </Theme>
     ),
   ],
 };
 
 export default meta;
-
-const ThemeOne = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <Theme
-      tokens={{
-        default: {
-          color: {
-            primary: "#0f0",
-          },
-          components: {
-            button: {
-              radius: "full",
-            },
-          },
-        },
-      }}
-      scoped>
-      {children}
-    </Theme>
-  );
-};
-
-const ThemeTwo = ({ children }: { children: ({ setTheme: any, theme: any, setTokens: any }) => React.ReactNode }) => {
-  const { setTheme, theme, setTokens } = useTheme();
-  return (
-    <Theme<"space" | "default">
-      defaultTheme="space"
-      tokens={{
-        space: {
-          color: {
-            primary: "#101",
-          },
-        },
-        default: {
-          color: {
-            primary: "#00a",
-          },
-          components: {
-            button: {
-              radius: "full",
-            },
-          },
-        },
-      }}
-      scoped>
-      {children({ setTheme, theme, setTokens })}
-    </Theme>
-  );
-};
-function Aux(): React.ReactElement {
-  return (
-    <>
-      <Card.Header>
-        <Text>Card Header</Text>
-      </Card.Header>
-      <Card.Body>
-        <Button>Button</Button>
-        <ThemeOne>
-          <Card>
-            <Card.Body>
-              <Button>Button</Button>
-              <ThemeTwo>
-                {({ setTheme, setTokens, theme }) => (
-                  <Card>
-                    <Card.Body>
-                      <Button
-                        onClick={() => {
-                          setTheme(theme === "space" ? "default" : "space");
-                          console.log("Click", theme);
-                        }}>
-                        Button
-                      </Button>
-                    </Card.Body>
-                  </Card>
-                )}
-              </ThemeTwo>
-            </Card.Body>
-          </Card>
-        </ThemeOne>
-        <Text>
-          Pellentesque elementum diam sapien, nec ultrices risus convallis eget. Nam pharetra dolor
-          at dictum tempor. Quisque ut est a ligula hendrerit sodales. Curabitur ornare a nulla in
-          laoreet. Maecenas semper mi egestas, dignissim nisi et, elementum neque.
-        </Text>
-      </Card.Body>
-      <Card.Footer>
-        <Text>Card Footer</Text>
-      </Card.Footer>
-    </>
-  );
-}
 
 export const Base: Story = {
   argTypes: {
@@ -115,13 +26,26 @@ export const Base: Story = {
       control: false,
     },
   },
-  render: () => {
-    return <Theme><Aux /></Theme>
-  },
   args: {
     selected: false,
     skin: "default",
-    children: <Aux />,
+    children: (
+      <>
+        <Card.Header>
+          <Text>Card Header</Text>
+        </Card.Header>
+        <Card.Body>
+          <Text>
+            Pellentesque elementum diam sapien, nec ultrices risus convallis eget. Nam pharetra
+            dolor at dictum tempor. Quisque ut est a ligula hendrerit sodales. Curabitur ornare a
+            nulla in laoreet. Maecenas semper mi egestas, dignissim nisi et, elementum neque.
+          </Text>
+        </Card.Body>
+        <Card.Footer>
+          <Text>Card Footer</Text>
+        </Card.Footer>
+      </>
+    ),
   },
 };
 
