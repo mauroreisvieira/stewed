fed560d8937f234be838d236b3d92c7fad62fbd0
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2024-05-15 16:19:25 +0100

    refactor(dropdown): support controlled and uncontrolled component

--- packages/react/src/dropdown/src/Dropdown.tsx
+++ packages/react/src/dropdown/src/Dropdown.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Scope } from "../../scope";
 // Hooks
 import { useBem, useFloating, useClickOutside, type FloatingPlacement } from "@stewed/hooks";
@@ -7,24 +7,51 @@ import { components } from "@stewed/tokens";
 // Styles
 import styles from "./styles/index.module.scss";
 
-export interface DropdownProps<T> extends React.ComponentPropsWithRef<"div"> {
-  /** The reference element for positioning the dropdown. */
-  reference: T | null;
-  /** The preferred placement of the dropdown. */
+export interface DropdownChildrenProps<T> {
+  /** Ref to attach to the `Dropdown` element */
+  ref: React.Ref<T>;
+  /** Event handler for mouse click */
+  onClick: React.MouseEventHandler<T>;
+}
+
+export interface DropdownProps<T>
+  extends Omit<React.ComponentPropsWithRef<"div">, "children" | "content"> {
+  /**
+   * Specifies the preferred placement of the `Dropdown` relative to its trigger.
+   * @example "top", "bottom", "left", "right"
+   */
   placement?: FloatingPlacement;
-  /** Determines if the dropdown is open. */
+  /** Indicates whether the `Dropdown` is currently open. */
   open?: boolean;
+  /**
+   * Allows the `Dropdown` to remain open even when clicking outside of it.
+   * @default false
+   */
+  allowClickOutside?: boolean;
   /** Callback function invoked when the dialog is clicked outside. */
   onClickOutside?: () => void;
+  /**
+   * Defines the content to be displayed inside the `Dropdown`.
+   */
+  content: React.ReactNode;
+  /**
+   * Function that returns a React element with events to trigger `Dropdown` position and visibility.
+   * @param props - Render props for `Dropdown` component.
+   */
+  children: (props: DropdownChildrenProps<T>) => React.ReactElement;
 }
 
 /**
  * Dropdown component is a floating element designed to serve as a lightweight context menu,
- * perfect for containing navigation options and action items within a user interface
+ * perfect for containing navigation options and action items within a user interface.
  *
  * @example
  * ```tsx
- * <Dropdown placement="top">Your Content Place</Dropdown>
+ * <Dropdown<HTMLButtonElement> placement="top" content="Surprise surprise, the king is back...">
+ *   {(props) => (
+ *     <button {...props}>Conor McGregor</button>
+ *   )}
+ * </Dropdown>
  * ```
  *
  * @remarks This component props extended from React.ComponentPropsWithRef<"div">.
@@ -34,11 +61,12 @@ export interface DropdownProps<T> extends React.ComponentPropsWithRef<"div"> {
  */
 export function Dropdown<T extends HTMLElement>({
   placement = "bottom-start",
-  reference,
   open,
   className,
   style,
+  allowClickOutside = false,
   onClickOutside,
+  content,
   children,
   ...props
 }: DropdownProps<T>): React.ReactElement {
@@ -50,24 +78,40 @@ export function Dropdown<T extends HTMLElement>({
     root: getBlock({ extraClasses: className }),
   };
 
+  // Create a reference to manage the dropdown element
+  const dropdownRef = useRef<T>(null);
+
+  const [isOpen, setOpen] = useState(open);
+
+  // Determines the visibility of the Dropdown based on controlled (open) or uncontrolled (isOpen) state.
+  const isVisible = open || isOpen;
+
   // Floating position calculation hook
   const { floating, x, y, isPositioned } = useFloating<T, HTMLDivElement>({
-    open,
+    open: isVisible,
     placement,
-    reference,
+    reference: dropdownRef.current,
     offset: 2,
   });
 
   // Hook to handle clicks outside the floating element.
   useClickOutside({
-    enabled: open,
-    ignoredElements: [reference as Element, floating.current as Element],
-    onClickOutside: () => onClickOutside?.(),
+    enabled: isVisible,
+    ignoredElements: [dropdownRef.current as Element, floating.current as Element],
+    onClickOutside: () => (allowClickOutside ? onClickOutside : setOpen(false)),
   });
+
+  const onHandleOpen = (): void => {
+    setOpen((prev) => !prev);
+  };
 
   return (
     <>
-      {open && (
+      {children?.({
+        ref: dropdownRef,
+        onClick: onHandleOpen,
+      })}
+      {isVisible && (
         <Scope elevation="navigation">
           <div
             ref={floating}
@@ -80,7 +124,7 @@ export function Dropdown<T extends HTMLElement>({
             }}
             {...props}
           >
-            {children}
+            {content}
           </div>
         </Scope>
       )}
