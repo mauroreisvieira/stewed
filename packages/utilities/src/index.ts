180ab527554620a5d67cd83f937376fa86407324
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2024-05-15 16:20:08 +0100

    feat: add utility to invert color

--- packages/utilities/src/index.ts
+++ packages/utilities/src/index.ts
@@ -3,4 +3,5 @@ export * from "./clickOutside";
 export * from "./clickOutsideManager";
 export * from "./focusTrap";
 export * from "./objectUtils";
+export * from "./invertColor";
 export * from "./platform";
