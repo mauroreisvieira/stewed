f34cacd6a43262a3f71361fc0a4ffc25317c8419
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2024-05-15 16:20:19 +0100

    docs: add documentation for dropdown component

---
+++ packages/react/src/dropdown/stories/Dropdown.stories.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+// Components
+import { Button, Theme, Dropdown, Box, ListBox, Separator } from "../../index";
+
+type Story = StoryObj<typeof Dropdown>;
+
+const meta: Meta<typeof Dropdown> = {
+  title: "Components/Dropdown",
+  component: Dropdown,
+  decorators: [
+    (Story) => (
+      <Theme>
+        <Box justify="center" items="center" padding={{ block: "7xl", inline: "7xl" }} grow>
+          <Story />
+        </Box>
+      </Theme>
+    ),
+  ],
+};
+
+export default meta;
+
+export const Base: Story = {
+  args: {
+    placement: "bottom-start",
+  },
+  argTypes: {
+    content: {
+      control: false,
+    },
+    placement: {
+      options: [
+        "top",
+        "top-start",
+        "top-end",
+        "right",
+        "right-start",
+        "right-end",
+        "bottom",
+        "bottom-start",
+        "bottom-end",
+        "left",
+        "left-start",
+        "left-end",
+      ],
+    },
+  },
+  render: (args) => {
+    return (
+      <Dropdown<HTMLButtonElement>
+        {...args}
+        content={
+          <ListBox>
+            <ListBox.Item rightSlot="⌘C">Copy</ListBox.Item>
+            <ListBox.Item rightSlot="⌘V">Past</ListBox.Item>
+            <Separator space={{ block: "xs" }} />
+            <ListBox.Item rightSlot="⌘Z">Undo</ListBox.Item>
+            <ListBox.Item rightSlot="⌘Y">Redo</ListBox.Item>
+          </ListBox>
+        }
+      >
+        {(props) => {
+          return <Button {...props}>Edit</Button>;
+        }}
+      </Dropdown>
+    );
+  },
+};
--- src/stories/floating/Floating.stories.tsx
+++ src/stories/floating/Floating.stories.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 // UI Components
 import {
   Container,
@@ -35,8 +35,6 @@ export default meta;
 export const EditProfile = {
   render: function Example() {
     const [dialogOpen, setDialogOpen] = useState(false);
-    const [dropdownOpen, setDropdownOpen] = useState(false);
-    const ref = useRef<HTMLButtonElement>(null);
 
     return (
       <Container screen="md" alignment="center" padding={{ block: "7xl" }}>
@@ -114,28 +112,29 @@ export const EditProfile = {
               <Separator />
               <Dialog.Footer>
                 <Box justify="between" gap="md">
-                  <Button
-                    ref={ref}
-                    appearance="ghost"
-                    leftSlot={<TbDotsVertical />}
-                    size="sm"
-                    onClick={() => setDropdownOpen(!dropdownOpen)}
-                    iconOnly
-                  >
-                    Settings
-                  </Button>
                   <Dropdown
-                    reference={ref.current}
-                    open={dropdownOpen}
-                    onClickOutside={() => setDropdownOpen(!dropdownOpen)}
+                    placement="right"
+                    content={
+                      <ListBox>
+                        <ListBox.Group>
+                          <ListBox.Item>Option 1</ListBox.Item>
+                          <ListBox.Item>Option 2</ListBox.Item>
+                          <ListBox.Item>Option 3</ListBox.Item>
+                        </ListBox.Group>
+                      </ListBox>
+                    }
                   >
-                    <ListBox>
-                      <ListBox.Group>
-                        <ListBox.Item>Option 1</ListBox.Item>
-                        <ListBox.Item>Option 2</ListBox.Item>
-                        <ListBox.Item>Option 3</ListBox.Item>
-                      </ListBox.Group>
-                    </ListBox>
+                    {(props) => (
+                      <Button
+                        {...props}
+                        appearance="ghost"
+                        leftSlot={<TbDotsVertical />}
+                        size="sm"
+                        iconOnly
+                      >
+                        Settings
+                      </Button>
+                    )}
                   </Dropdown>
                   <Box justify="end" gap="md">
                     <Button appearance="outline">Cancel</Button>
