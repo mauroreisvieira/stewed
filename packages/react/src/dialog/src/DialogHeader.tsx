7192e97af7299d3b6d3471e194b40d4df59c86e1
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2024-07-12 16:28:41 +0100

    fix: remove unnecessary slots

--- packages/react/src/dialog/src/DialogHeader.tsx
+++ packages/react/src/dialog/src/DialogHeader.tsx
@@ -27,28 +27,26 @@ export function DialogHeader({
 
   return (
     <div className={cssClasses.root} {...props}>
-      <div>{children}</div>
-      <div>
-        {onClose && (
-          <Button
-            onClick={onClose}
-            size="sm"
-            appearance="ghost"
-            iconOnly
-            leftSlot={
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={2}
-                width={16}
-                stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-              </svg>
-            }
-          />
-        )}
-      </div>
+      {children}
+      {onClose && (
+        <Button
+          onClick={onClose}
+          size="sm"
+          appearance="ghost"
+          iconOnly
+          leftSlot={
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              width={16}
+              stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          }
+        />
+      )}
     </div>
   );
 }
