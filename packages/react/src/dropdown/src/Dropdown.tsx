75183082ca2392ea39ced316f9968a9e7af10c19
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2024-08-02 14:08:17 +0100

    chore: add comment in import

--- packages/react/src/dropdown/src/Dropdown.tsx
+++ packages/react/src/dropdown/src/Dropdown.tsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from "react";
-import { Scope } from "../../scope";
+// UI Components
+import { Scope } from "../..";
 // Hooks
 import { useBem, useFloating, useClickOutside, type FloatingPlacement } from "@stewed/hooks";
 // Tokens
