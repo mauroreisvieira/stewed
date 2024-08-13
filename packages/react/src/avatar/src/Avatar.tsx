680a82368fb0416aeb0722f6f39ba2c2144fc2f4
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2024-08-13 15:32:33 +0100

    feat: add forwardRef in avatar component

--- packages/react/src/avatar/src/Avatar.tsx
+++ packages/react/src/avatar/src/Avatar.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { forwardRef, useCallback, useState } from "react";
 // Compound Component
 import { AvatarGroup } from "./AvatarGroup";
 // Hooks
@@ -9,6 +9,8 @@ import { type Color, components } from "@stewed/tokens";
 import styles from "./styles/index.module.scss";
 
 export interface AvatarProps extends React.ComponentPropsWithRef<"div"> {
+  /** The ref to attach to the root `div` element. */
+  rootRef?: React.Ref<HTMLDivElement>;
   /**
    * Defines the skin color of the avatar.
    * @default primary
@@ -47,45 +49,53 @@ export interface AvatarProps extends React.ComponentPropsWithRef<"div"> {
  * @param {AvatarProps} props - The props for the Avatar component.
  * @returns {React.ReactElement} - The rendered Avatar component.
  */
-export function Avatar({
-  size = "md",
-  skin = "primary",
-  appearance = "circle",
-  className,
-  src,
-  name,
-  ...props
-}: AvatarProps): React.ReactElement {
-  // Importing useBem to handle BEM class names
-  const { getBlock, getElement } = useBem({ block: components.Avatar, styles });
+export const Root = forwardRef(
+  (
+    {
+      rootRef,
+      size = "md",
+      skin = "primary",
+      appearance = "circle",
+      className,
+      src,
+      name,
+      ...props
+    }: AvatarProps,
+    ref: React.Ref<HTMLImageElement>,
+  ): React.ReactElement => {
+    // Importing useBem to handle BEM class names
+    const { getBlock, getElement } = useBem({ block: components.Avatar, styles });
 
-  // Generating CSS classes based on component props and styles
-  const cssClasses = {
-    root: getBlock({ modifiers: [appearance, size, skin], extraClasses: className }),
-    img: getElement(["img"]),
-  };
+    // Generating CSS classes based on component props and styles
+    const cssClasses = {
+      root: getBlock({ modifiers: [appearance, size, skin], extraClasses: className }),
+      img: getElement(["img"]),
+    };
 
-  // State to track if there is an error loading the image
-  const [imageError, setImageError] = useState(false);
+    // State to track if there is an error loading the image
+    const [imageError, setImageError] = useState(false);
 
-  // Extract initials from the given name, using only the first two uppercase letters
-  const initials = name?.match(/[A-Z]/g)?.join("").slice(0, 2).toUpperCase();
+    // Extract initials from the given name, using only the first two uppercase letters
+    const initials = name?.match(/[A-Z]/g)?.join("").slice(0, 2).toUpperCase();
 
-  // Callback to handle image load errors, setting the error state
-  const onHandleError = useCallback<React.ReactEventHandler<HTMLImageElement>>(() => {
-    setImageError(true);
-  }, []);
+    // Callback to handle image load errors, setting the error state
+    const onHandleError = useCallback<React.ReactEventHandler<HTMLImageElement>>(() => {
+      setImageError(true);
+    }, []);
 
-  return (
-    <div className={cssClasses.root} {...props}>
-      {!imageError && src ? (
-        <img className={cssClasses.img} src={src} alt={name} onError={onHandleError} />
-      ) : (
-        initials
-      )}
-    </div>
-  );
-}
+    return (
+      <div ref={rootRef} className={cssClasses.root} {...props}>
+        {!imageError && src ? (
+          <img ref={ref} className={cssClasses.img} src={src} alt={name} onError={onHandleError} />
+        ) : (
+          initials
+        )}
+      </div>
+    );
+  },
+);
 
 // Compound component composition
-Avatar.Group = AvatarGroup;
+export const Avatar = Object.assign(Root, {
+  Group: AvatarGroup,
+});
