b5f71cccb9559e395a68eaf7bbb0639befbd304c
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2024-08-13 12:45:24 +0100

    chore: update style lint configurations

--- packages/react/src/drawer/stories/Drawer.stories.tsx
+++ packages/react/src/drawer/stories/Drawer.stories.tsx
@@ -46,3 +46,36 @@ export const Base: Story = {
     ),
   },
 };
+
+
+export const HugeContent: Story = {
+  argTypes: {
+    children: {
+      control: false,
+    },
+  },
+  args: {
+    open: false,
+    children: (
+      <>
+        <Drawer.Header>
+          <Text as="h4">Drawer Header</Text>
+        </Drawer.Header>
+        <Separator />
+        <Drawer.Body>
+          {Array.from({ length: 100 }).map((_, index) => (
+            <Text key={index}>
+              Pellentesque elementum diam sapien, nec ultrices risus convallis eget. Nam pharetra
+              dolor at dictum tempor. Quisque ut est a ligula hendrerit sodales. Curabitur ornare a
+              nulla in laoreet. Maecenas semper mi egestas, dignissim nisi et, elementum neque.
+            </Text>
+          ))}
+        </Drawer.Body>
+        <Separator />
+        <Drawer.Footer>
+          <Text as="h4">Drawer Footer</Text>
+        </Drawer.Footer>
+      </>
+    ),
+  },
+};
--- stylelint.config.js
+++ stylelint.config.js
@@ -1,15 +1,31 @@
-const { getStyleLintConfig } = require("@harmonix-hub/stylelint");
-
-const baseConfig = getStyleLintConfig();
-
 /** @type {import("stylelint").Config} */
 module.exports = {
-  /**
-   * Use main configuration from shared infrastructure
-   */
-  ...baseConfig,
+  customSyntax: "postcss-scss",
+  extends: [
+    "stylelint-config-recommended",
+    "stylelint-config-standard",
+    "stylelint-config-recommended-scss",
+    "stylelint-config-standard-scss",
+    "stylelint-config-prettier-scss",
+    "stylelint-config-recess-order",
+  ],
+  plugins: ["stylelint-scss", "stylelint-prettier", "stylelint-no-unsupported-browser-features"],
   rules: {
-    ...baseConfig.rules,
+    /**
+     * Rules
+     */
+    "at-rule-no-unknown": null,
+    "selector-no-qualifying-type": true,
+    "selector-no-vendor-prefix": true,
+    "scss/at-rule-no-unknown": true,
+    "selector-max-type": 0,
+    "max-nesting-depth": 4,
+    "selector-max-id": 0,
+    // Regex to allow BEM selector
+    "selector-class-pattern": "^([a-z][a-z0-9]*)((--?|__)?[a-z0-9]+)*$",
+    /**
+     * Browser compatibility
+     */
     "plugin/no-unsupported-browser-features": [
       true,
       {
@@ -29,5 +45,23 @@ module.exports = {
         ignore: ["css-nesting", "css-when-else"],
       },
     ],
+    /**
+     * Formatting
+     */
+    "rule-empty-line-before": [
+      "always-multi-line",
+      {
+        except: ["first-nested"],
+        ignore: ["after-comment"],
+      },
+    ],
+    "function-calc-no-unspaced-operator": null,
+    "prettier/prettier": [
+      true,
+      {
+        endOfLine: "lf",
+      },
+    ],
+    "scss/map-keys-quotes": "always",
   },
 };
