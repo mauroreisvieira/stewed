a1bfc5fdae390227c6853e71912065c15c8e9562
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2024-08-16 16:17:52 +0100

    feat: more sizes for skeleton component

--- packages/react/src/skeleton/stories/Skeleton.stories.tsx
+++ packages/react/src/skeleton/stories/Skeleton.stories.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 // UI Components
-import { Theme, Skeleton } from "../../index";
+import { Theme, Grid, Skeleton, AspectRatio, Table, Card, Stack, Separator } from "../../index";
 
 type Story = StoryObj<typeof Skeleton>;
 
@@ -25,9 +25,66 @@ export const Base: Story = {
       control: false,
     },
   },
-  args: {
-    shape: "line",
-    size: "lg",
-    radius: "md",
+};
+
+export const CardMedia: Story = {
+  argTypes: {
+    children: {
+      control: false,
+    },
+  },
+  render: function Render() {
+    return (
+      <Grid cols={1} responsive={{ sm: { cols: 3 } }}>
+        <Card>
+          <AspectRatio ratio="16:9">
+            <Skeleton radius="none" size="auto" />
+          </AspectRatio>
+          <Separator />
+          <Card.Body>
+            <Stack direction="column" gap="sm">
+              <Skeleton />
+              <Skeleton size="xs" />
+              <Skeleton size="xs" />
+            </Stack>
+          </Card.Body>
+        </Card>
+      </Grid>
+    );
+  },
+};
+
+export const DataTable: Story = {
+  argTypes: {
+    children: {
+      control: false,
+    },
+  },
+  render: function Render() {
+    return (
+      <Table appearance={["border", "border-rows", "border-columns"]}>
+        <Table.Head>
+          <Table.Row>
+            {Array.from({ length: 4 }).map((_, cellIndex) => (
+              <Table.Cell key={cellIndex} as="th">
+                <Skeleton size="md" />
+              </Table.Cell>
+            ))}
+          </Table.Row>
+        </Table.Head>
+
+        <Table.Body>
+          {Array.from({ length: 6 }).map((_, rowIndex) => (
+            <Table.Row key={rowIndex}>
+              {Array.from({ length: 4 }).map((_, cellIndex) => (
+                <Table.Cell key={cellIndex}>
+                  <Skeleton size="sm" style={{ width: "50%" }} />
+                </Table.Cell>
+              ))}
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+    );
   },
 };
