a70786fad1b149a5f63586366fa552f8bc9e21dc
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2024-10-08 15:23:40 +0100

    docs: update hooks documentation

--- packages/hooks/src/use-async/src/useAsync.ts
+++ packages/hooks/src/use-async/src/useAsync.ts
@@ -2,19 +2,35 @@ import { useState, useCallback, useEffect } from "react";
 
 type Status = "idle" | "pending" | "success" | "error";
 
+interface UseAsync<T> {
+  /**
+   * Function to execute the asynchronous operation.
+   * @returns A promise that resolves when the operation is complete.
+   */
+  execute: () => Promise<void>;
+  /**
+   * The current status of the async operation.
+   * - "idle": Initial state, no operation in progress.
+   * - "pending": The operation is currently being executed.
+   * - "success": The operation completed successfully.
+   * - "error": The operation failed with an error.
+   */
+  status: Status;
+  /**  The result of the asynchronous operation, or null if it hasn't completed or failed. */
+  value: T | null;
+  /** The error encountered during the asynchronous operation, or null if there was no error. */
+  error: Error | null;
+}
+
 /**
- * Hook to handle asynchronous operations.
+ * Hook to manage asynchronous operations with state handling.
  *
  * @template T The type of the result returned by the async function.
- * @param asyncFunction The asynchronous function to be executed.
- * @param immediate If true, the asyncFunction is executed immediately after the hook is mounted. Default is true.
- * @returns An object containing:
- * - `execute`: A function to manually trigger the async function.
- * - `status`: The current status of the async operation ('idle', 'pending', 'success', 'error').
- * - `value`: The result of the async operation (if successful).
- * - `error`: The error object (if the async operation fails).
+ * @param asyncFunction A function that returns a promise to be executed asynchronously.
+ * @param immediate Optional parameter that determines if the asyncFunction should be executed immediately after the hook is mounted. Defaults to true.
+ * @returns An object containing the execute function, current status, result value, and any encountered error.
  */
-export function useAsync<T>(asyncFunction: () => Promise<T>, immediate = true) {
+export function useAsync<T>(asyncFunction: () => Promise<T>, immediate = true): UseAsync<T> {
   const [status, setStatus] = useState<Status>("idle");
   const [value, setValue] = useState<T | null>(null);
   const [error, setError] = useState<Error | null>(null);
