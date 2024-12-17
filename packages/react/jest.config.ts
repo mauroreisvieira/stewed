0db315d0df4b8ed5b2d2f7a0be406e7bca9222f7
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2024-12-17 15:43:52 +0000

    chore: update dependencies

--- packages/react/jest.config.ts
+++ packages/react/jest.config.ts
@@ -4,7 +4,6 @@ module.exports = {
   ...jestConfig,
   testEnvironment: "jsdom",
   moduleNameMapper: {
-    "^.+\\.(ts|tsx)?$": "ts-jest",
     "\\.(css|less|scss|sss|styl)$": "<rootDir>/node_modules/jest-css-modules"
   }
 };
