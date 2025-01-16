772978c57fa2f3f015b1dbba0db2fc33b4c7da22
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2025-01-16 18:48:35 +0000

    test: update unit tests

--- packages/utilities/src/platform.ts
+++ packages/utilities/src/platform.ts
@@ -4,10 +4,10 @@
  * @param re - The regular expression to test.
  * @returns A boolean indicating whether the regular expression matches the user agent string.
  */
-function testUserAgent(re: RegExp): boolean {
-  return typeof window !== "undefined" && window.navigator
-    ? re.test(window.navigator.userAgent)
-    : false;
+export function testUserAgent(re: RegExp): boolean {
+  if (typeof window === "undefined" || !window.navigator) return false;
+
+  return re.test(window.navigator.userAgent);
 }
 
 /**
@@ -22,14 +22,14 @@ export const isBrowser = (): boolean => typeof window !== "undefined";
  *
  * @returns A boolean indicating whether the current platform is macOS.
  */
-export const isMac = (): boolean => testUserAgent(/^Mac/);
+export const isMac = (): boolean => testUserAgent(/Macintosh/);
 
 /**
  * Checks if the current platform is iPhone.
  *
  * @returns A boolean indicating whether the current platform is iPhone.
  */
-export const isIPhone = (): boolean => testUserAgent(/^iPhone/);
+export const isIPhone = (): boolean => testUserAgent(/iPhone/);
 
 /**
  * Checks if the current platform is iPad.
@@ -37,7 +37,8 @@ export const isIPhone = (): boolean => testUserAgent(/^iPhone/);
  * @returns A boolean indicating whether the current platform is iPad.
  */
 export const isIPad = (): boolean =>
-  testUserAgent(/^iPad/) || (isMac() && navigator.maxTouchPoints > 1);
+  testUserAgent(/iPad/) ||
+  (isMac() && typeof navigator !== "undefined" && navigator.maxTouchPoints > 1);
 
 /**
  * Checks if the current platform is iOS.
@@ -58,7 +59,7 @@ export const isAppleDevice = (): boolean => isMac() || isIOS();
  *
  * @returns A boolean indicating whether the current browser is Chrome.
  */
-export const isChrome = (): boolean => testUserAgent(/Chrome/);
+export const isChrome = (): boolean => testUserAgent(/Chrome/) && !testUserAgent(/Edge|OPR/);
 
 /**
  * Checks if the current browser engine is WebKit.
@@ -75,30 +76,23 @@ export const isWebKit = (): boolean => testUserAgent(/AppleWebKit/) && !isChrome
 export const isAndroid = (): boolean => testUserAgent(/Android/);
 
 /**
- * Checks if the current platform is touch.
+ * Checks if the current platform supports touch.
  *
- * @returns A boolean indicating whether the current platform is touch.
+ * @returns A boolean indicating whether the current platform supports touch.
  */
 export const isTouch = (): boolean =>
-  typeof window !== "undefined" && ("ontouchstart" in window || navigator.maxTouchPoints > 0);
+  typeof window !== "undefined" &&
+  ("ontouchstart" in window || (typeof navigator !== "undefined" && navigator.maxTouchPoints > 0));
 
 /**
  * Checks if the code is running in a client-side environment (browser).
  *
- * This function verifies the presence of the `window` object, `document`,
- * and the `createElement` method, which are specific to the browser environment.
- *
- * @returns {boolean} `true` if the code is running on the client-side, otherwise `false`.
- *
- * @example
- * if (isClient()) {
- *   console.log("Running in the browser!");
- * }
+ * @returns A boolean indicating whether the code is running on the client-side.
  */
 export const isClient = (): boolean => {
   return (
     typeof window !== "undefined" &&
-    typeof window.document !== "undefined" &&
-    typeof window.document.createElement === "function"
+    typeof document !== "undefined" &&
+    typeof document.createElement === "function"
   );
 };
