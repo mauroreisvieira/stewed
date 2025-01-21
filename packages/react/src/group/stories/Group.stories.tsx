360de92f94414bb4a84e73643059a439ad3f51bb
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2025-01-21 14:18:57 +0000

    fix(group): apply flex wrap for group when `gap` is defined

--- packages/react/src/group/stories/Group.stories.tsx
+++ packages/react/src/group/stories/Group.stories.tsx
@@ -2,7 +2,7 @@ import React from "react";
 // Storybook
 import type { Meta, StoryObj } from "@storybook/react";
 // Components
-import { Theme, Group, Button, TextField } from "../../index";
+import { Theme, Group, Box, Button, TextField, Text, Icon } from "../../index";
 
 type Story = StoryObj<typeof Group>;
 
@@ -52,16 +52,40 @@ export const TextFields: Story = {
 
 export const Mixed: Story = {
   args: {
-    children: (
-      <>
-        <Button skin="primary" appearance="soft">
-          Button
-        </Button>
-        <TextField appearance="outline" defaultValue={"Input field..."} />
-        <Button skin="primary" appearance="soft">
-          Button
-        </Button>
-      </>
-    )
+    gap: "md"
+  },
+  render: function Render(args): React.ReactElement {
+    return (
+      <Box
+        inline
+        radius="md"
+        borderColor="neutral-faded"
+        borderStyle="solid"
+        borderWidth={1}
+        padding={{ block: "xs", inline: "xs" }}
+      >
+        <Group {...args}>
+          <Button
+            size="sm"
+            skin="neutral"
+            appearance="ghost"
+            leftSlot={<Icon.Minus size={12} />}
+            iconOnly
+          >
+            Decrease
+          </Button>
+          <Text size="sm">10</Text>
+          <Button
+            size="sm"
+            skin="neutral"
+            appearance="ghost"
+            leftSlot={<Icon.Plus size={14} />}
+            iconOnly
+          >
+            Increase
+          </Button>
+        </Group>
+      </Box>
+    );
   }
 };
