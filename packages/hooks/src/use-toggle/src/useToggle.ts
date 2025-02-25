78dfca652ed5adb90ffd074287b6a5f6c063234f
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2025-02-25 10:16:26 +0000

    fix(use-toggle): update return methods in hook

--- packages/hooks/src/use-toggle/src/useToggle.ts
+++ packages/hooks/src/use-toggle/src/useToggle.ts
@@ -1,29 +1,63 @@
-import { useCallback, useState, type Dispatch, type SetStateAction } from "react";
+import { useMemo, useState, type Dispatch, type SetStateAction } from "react";
 
 /**
- * Hook that manages a boolean state and provides a function to toggle it.
+ * Interface for managing a boolean toggle state.
+ */
+interface IUseToggle {
+  /** The current state of the toggle. */
+  isOn: boolean;
+  /** Sets the toggle state to a specific value. */
+  set: Dispatch<SetStateAction<boolean>>;
+  /** Turns the toggle on (`true`). */
+  on: () => void;
+  /** Turns the toggle off (`false`). */
+  off: () => void;
+  /** Toggles the state between `true` and `false`. */
+  toggle: () => void;
+  /** Resets the toggle to its initial state. */
+  reset: () => void;
+}
+
+/**
+ * Hook that manages a boolean state with utility functions to modify it.
  *
- * @param initialState - The initial state value. Default is `false`.
- * @returns A tuple containing the current state value and a function to toggle the state.
+ * @param defaultState - The initial state value.
+ * @returns An object containing the current state and functions to manipulate it.
  *
  * @example
  * ```tsx
- * const [isToggled, onToggle] = useToggle();
+ * const { isOn, toggle } = useToggle();
  *
  * return (
  *   <div>
  *     <button onClick={toggle}>Toggle</button>
- *     <p>The state is: {isToggled ? 'On' : 'Off'}</p>
+ *     <p>The state is: {isOn ? 'On' : 'Off'}</p>
  *   </div>
  * );
- *
  * ```
  */
-export function useToggle(
-  initialState = false
-): [boolean, () => void, Dispatch<SetStateAction<boolean>>] {
-  const [state, setState] = useState<boolean>(initialState);
-  const onToggle = useCallback((): void => setState((state) => !state), []);
+export function useToggle(defaultState = false): IUseToggle {
+  // initialize the initial state from the provided default value
+  const [initState] = useState(defaultState);
 
-  return [state, onToggle, setState];
+  // create a state variable to track the toggle's current state
+  const [isOn, setOn] = useState(initState);
+
+  return useMemo(
+    () => ({
+      /** current state of the toggle */
+      isOn,
+      /** function to manually set the toggle state */
+      set: setOn,
+      /** sets the toggle state to `true` */
+      on: (): void => setOn(true),
+      /** sets the toggle state to `false` */
+      off: (): void => setOn(false),
+      /** toggles the state between `true` and `false` */
+      toggle: (): void => setOn((prevIsOn) => !prevIsOn),
+      /** resets the toggle to its initial state */
+      reset: (): void => setOn(initState)
+    }),
+    [initState, isOn]
+  );
 }
